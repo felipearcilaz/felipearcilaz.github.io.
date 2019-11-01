c031562528b0d9dd444eb1f772e21d2780b93abc
Felipe Arcila <felipearcilaz@gmail.com>
2019-10-31 20:10:13 -0700

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,129 @@
+let resumeData = {
+  // "imagebaseurl":"https://techonoeticsclub.github.io/",  MAKE FOR MYSELF
+  name: "Felipe Arcila",
+  role:
+    " a problem solver, with a knack for pleasing aesthetics and functionality.",
+  //linkedinId: "felipearcila",
+ // skypeid: "felipearcila87@hotmail.com",
+  roleDescription: "Front-End Developer, Project Manager.",
+
+  socialLinks: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/felipearcila/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "Github",
+      url: "https://github.com/felipearcilaz?tab=repositories",
+      className: "fa fa-github"
+    },
+    {
+      name: "Skype",
+      url: "http://skype.com",
+      className: "fa fa-skype"
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/felipe87hz/",
+      className: "fa fa-instagram"
+    }
+  ],
+
+  aboutme:
+    "Strategic, self-motivated Front End Developer with experience in Project Management. Possess a range of software development skills. Actively seeking strategic Software Development opportunities. Willing to relocate.",
+  address: "Huntington Beach, CA",
+  website: "https://www.https://github.com/felipearcilaz.com",
+  education: [
+    {
+      UniversityName: "",
+      specialization: "Full Stack Development Bootcamp",
+      MonthOfPassing: "Sept",
+      YearOfPassing: "2019"
+      //"Achievements":""
+    },
+    {
+      UniversityName: "McGill University",
+      specialization: "Masters in Management",
+      MonthOfPassing: "Jul",
+      YearOfPassing: "2016"
+      //"Achievements":"Some Achievements"
+    },
+    {
+      UniversityName: "Zhejiang University",
+      specialization: "MBA",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2016"
+      //"Achievements":"Some Achievements"
+    },
+    {
+      UniversityName: "Cal State Long Beach",
+      specialization: "International Studies",
+      MonthOfPassing: "Jul",
+      YearOfPassing: "2011"
+      //"Achievements":"Some Achievements"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "Reactjs"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Victoria Zapata, Life Coach"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Ms. Julia Pritchett"
+    }
+  ]
+};
+
+export default resumeData;
